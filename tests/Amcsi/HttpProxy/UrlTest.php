735675dce548f70fa4bd7f296cdcc648a12c6788
Attila Szeremi <attila.szeremi@netefficiency.co.uk>
2014-02-10 15:15:08 +0000

    Cookie handling fixed

--- tests/Amcsi/HttpProxy/UrlTest.php
+++ tests/Amcsi/HttpProxy/UrlTest.php
@@ -21,6 +21,25 @@ class UrlTest extends PHPUnit_Framework_TestCase
         $host = 'source-url.com';
         $result = $this->url->getRewriteDetails($reqUri, $host);
         $expected = array(
+            'proxyPath' => '/a/b/c/',
+            'proxyHostPath' => 'source-url.com/a/b/c/',
+            'proxyProtocolHostPath' => 'http://source-url.com/a/b/c/',
+            'trueHostPathQuery' => 'target-url.com/d/e/',
+            'trueScheme' => 'http',
+        );
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetRewriteDetailsWithQuery()
+    {
+        $this->url = $url = new Amcsi_HttpProxy_Url(
+            'http://target-url.com/d/e/x/y/z.php?foo=bar'
+        );
+        $reqUri = '/a/b/c/x/y/z.php?foo=bar';
+        $host = 'source-url.com';
+        $result = $this->url->getRewriteDetails($reqUri, $host);
+        $expected = array(
+            'proxyPath' => '/a/b/c/',
             'proxyHostPath' => 'source-url.com/a/b/c/',
             'proxyProtocolHostPath' => 'http://source-url.com/a/b/c/',
             'trueHostPathQuery' => 'target-url.com/d/e/',
