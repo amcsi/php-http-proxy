0c71530c9c24d36fd2afa0e56fd12a941ad988d7
Attila Szeremi <amcsi@mailbox.hu>
2014-02-17 18:12:12 +0000

    Deprecated some methods. Tests most be rewritten.
    Non-apache rewrite style proxying works. A-Rewrite style doesn't.
    Cookies may not work either.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
+if (!defined('E_DEPRECATED')) {
+    define('E_DEPRECATED', 8192);
+}
+if (!defined('E_USER_DEPRECATED')) {
+    define('E_USER_DEPRECATED', 16384);
+}
 $basePath = realpath(dirname(__FILE__) . '/..');
 require_once "$basePath/vendor/autoload.php";
