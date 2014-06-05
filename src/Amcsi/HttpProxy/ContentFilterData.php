f9212dad874ba99f5538853904c2c55c54015d67
Szerémi Attila <amcsi@mailbox.hu>
2014-06-05 15:21:06 +0100

    Forgot to add ContentFilterData.php

---
+++ src/Amcsi/HttpProxy/ContentFilterData.php
@@ -0,0 +1,33 @@
+<?php
+class Amcsi_HttpProxy_ContentFilterData
+{
+    private $request;
+    private $response;
+
+    /**
+     * __construct 
+     * 
+     * @param Amcsi_HttpProxy_Request $request 
+     * @param Amcsi_HttpProxy_Response $Amcsi_HttpProxy_Response 
+     * @access public
+     * @return void
+     */
+    public function __construct(
+        Amcsi_HttpProxy_Request $request,
+        Amcsi_HttpProxy_Response $response
+    ) {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    /**
+     * getResponseContent 
+     * 
+     * @access public
+     * @return string
+     */
+    public function getResponseContent()
+    {
+        return $this->response->getContent();
+    }
+}
